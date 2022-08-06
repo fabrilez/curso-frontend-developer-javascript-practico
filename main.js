dde684d235e60696e1583e22128371200ed5fb87
fabrilez <fabrilez2001@gmail.com>
2022-08-06 16:54:35 -0400

    Fusion de elementos y estilos del archivo clase 8 para desplegar el menu hamburguesa en vista mobile manipulandolo con JS

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
 const menuEmail = document.querySelector('.navbar-email');
-const DesktopMenu = document.querySelector('.desktop-menu')
+const DesktopMenu = document.querySelector('.desktop-menu');
+const menuHamburguer = document.querySelector('.menu');
+const listMenuHamburger = document.querySelector('.mobile-menu')
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
+menuHamburguer.addEventListener('click', toggleHamburguerMenu);
 
 function toggleDesktopMenu() {
     DesktopMenu.classList.toggle('inactive');
+}
+
+function toggleHamburguerMenu() {
+    listMenuHamburger.classList.toggle('inactive');
 }
