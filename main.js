0719d13e50026b57d485955144b32a74fe5cebbc
fabrilez <fabrilez2001@gmail.com>
2022-08-06 18:57:41 -0400

    fusion de los elementos y estilos del archivo clase 13 manipulandolo con JS

--- main.js
+++ main.js
@@ -1,15 +1,22 @@
 const menuEmail = document.querySelector('.navbar-email');
 const DesktopMenu = document.querySelector('.desktop-menu');
-const menuHamburguer = document.querySelector('.menu');
-const listMenuHamburger = document.querySelector('.mobile-menu')
+const burguerMenu = document.querySelector('.menu');
+const listBurguerMenu = document.querySelector('.mobile-menu');
+const shoppingCard = document.querySelector('.navbar-shopping-cart');
+const productDetail = document.querySelector('.product-detail');
 
+shoppingCard.addEventListener('click', toggleProductDetail)
 menuEmail.addEventListener('click', toggleDesktopMenu);
-menuHamburguer.addEventListener('click', toggleHamburguerMenu);
+burguerMenu.addEventListener('click', toggleBurguerMenu);
 
 function toggleDesktopMenu() {
     DesktopMenu.classList.toggle('inactive');
 }
 
-function toggleHamburguerMenu() {
-    listMenuHamburger.classList.toggle('inactive');
+function toggleBurguerMenu() {
+    listBurguerMenu.classList.toggle('inactive');
+}
+
+function toggleProductDetail() {
+    productDetail.classList.toggle('inactive')
 }
