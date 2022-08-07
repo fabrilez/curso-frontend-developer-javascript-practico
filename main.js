1a2c1eb8255229d7900abb105735fb6109324660
fabrilez <fabrilez2001@gmail.com>
2022-08-07 01:13:11 -0400

    fusion de los elementos y estilos de los archivos de la clase 6, inserte codigo HTML desde Javascript para agregar segun los productos que tengamos

--- main.js
+++ main.js
@@ -1,9 +1,33 @@
 const menuEmail = document.querySelector('.navbar-email');
 const DesktopMenu = document.querySelector('.desktop-menu');
+
 const burguerMenu = document.querySelector('.menu');
 const listBurguerMenu = document.querySelector('.mobile-menu');
+
 const shoppingCard = document.querySelector('.navbar-shopping-cart');
 const productDetail = document.querySelector('.product-detail');
+
+const cardsContainer = document.querySelector('.cards-container')
+
+const productList = [];
+
+productList.push({
+    name: 'Bike',
+    price: 120,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+})
+
+productList.push({
+    name: 'Pantalla',
+    price: 820,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+})
+
+productList.push({
+    name: 'Computadora',
+    price: 500,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+})
 
 shoppingCard.addEventListener('click', toggleProductDetail)
 menuEmail.addEventListener('click', toggleDesktopMenu);
@@ -11,12 +35,56 @@ burguerMenu.addEventListener('click', toggleBurguerMenu);
 
 function toggleDesktopMenu() {
     DesktopMenu.classList.toggle('inactive');
+    productDetail.classList.add('inactive');
 }
 
 function toggleBurguerMenu() {
     listBurguerMenu.classList.toggle('inactive');
+    productDetail.classList.add('inactive');
 }
 
 function toggleProductDetail() {
-    productDetail.classList.toggle('inactive')
+    productDetail.classList.toggle('inactive');
+    DesktopMenu.classList.add('inactive');
+    listBurguerMenu.classList.add('inactive');
 }
+
+
+function renderProducts(arr) {
+    for(product of arr) {
+        const productCard = document.createElement('div');
+        productCard.classList.add('product-card');
+    
+        const productImg = document.createElement('img');
+        productImg.setAttribute('src', product.image);
+    
+        const productInfo = document.createElement('div');
+        productInfo.classList.add('product-info');
+    
+        const productInfoDiv = document.createElement('div');
+    
+        const productPrice = document.createElement('p')
+        productPrice.innerText = '$' + product.price;
+        const productName = document.createElement('p')
+        productName.innerText = product.name;
+    
+        productInfoDiv.appendChild(productPrice);
+        productInfoDiv.appendChild(productName);
+    
+        const productInfoFigure = document.createElement('figure');
+        const productImgCart = document.createElement('img');
+        productImgCart.setAttribute('src', './icons/bt_add_to_cart.svg');
+    
+        productInfoFigure.appendChild(productImgCart);
+    
+        productInfo.appendChild(productInfoDiv);
+        productInfo.appendChild(productInfoFigure);
+    
+        productCard.appendChild(productImg);
+        productCard.appendChild(productInfo);
+    
+        cardsContainer.appendChild(productCard)
+    }
+}
+
+renderProducts(productList)
